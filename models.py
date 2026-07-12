17da21ef4a1938d9ebbf0e64f85bf6b006c3ed9a
Your Name <Your_email@example.com>
2026-07-12 23:18:40 +0530

    Update models

--- models.py
+++ models.py
@@ -3,18 +3,27 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+
 class User(UserMixin, db.Model):
+    __tablename__ = "user"
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    role = db.Column(db.String(20))  # student / admin
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default="student")
+
 
 class Course(db.Model):
+    __tablename__ = "course"
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    seats = db.Column(db.Integer)
+    name = db.Column(db.String(100), nullable=False)
+    seats = db.Column(db.Integer, nullable=False)
+
 
 class Registration(db.Model):
+    __tablename__ = "registration"
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=False)
